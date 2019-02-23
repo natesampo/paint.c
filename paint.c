1416e0389ec9c9eb2cae1ee1bbe5086af4f66e9c
Nate <nathan.sampo@students.olin.edu>
2019-02-23 15:25:34 -0500

    Added some code to resize window

--- paint.c
+++ paint.c
@@ -1,48 +1,70 @@
 #include <gtk/gtk.h>
- 
-static int counter = 0;
- 
-void greet( GtkWidget *widget, gpointer   data )
+
+static void print_hello(GtkWidget *widget, gpointer data)
 {
-   // printf equivalent in GTK+
-   g_print ("Hi there! Welcome to GTK\n");
-   g_print ("%s clicked %d times\n",
-      (char*)data, ++counter);
+	g_print ("Hello World\n");
 }
- 
-void destroy( GtkWidget *widget,gpointer   data )
+
+static void activate(GtkApplication *app, gpointer user_data)
 {
-   gtk_main_quit ();
+	GtkWidget *window;
+	GtkWidget *grid;
+	GtkWidget *button;
+
+	/* create a new window, and set its title */
+	window = gtk_application_window_new(app);
+	gtk_window_set_title (GTK_WINDOW(window), "Window");
+	gtk_window_set_default_size(GTK_WINDOW(window), 1000, 800);
+	gtk_window_set_resizable(GTK_WINDOW(window), FALSE);
+	gtk_container_set_border_width(GTK_CONTAINER (window), 10);
+
+	/* Here we construct the container that is going pack our buttons */
+	grid = gtk_grid_new ();
+
+	/* Pack the container in the window */
+	gtk_container_add (GTK_CONTAINER (window), grid);
+
+	button = gtk_button_new_with_label ("Button 1");
+	g_signal_connect(button, "clicked", G_CALLBACK(print_hello), NULL);
+
+	/* Place the first button in the grid cell (0, 0), and make it fill
+	* just 1 cell horizontally and vertically (ie no spanning)
+	*/
+	gtk_grid_attach (GTK_GRID (grid), button, 0, 0, 1, 1);
+
+	button = gtk_button_new_with_label ("Button 2");
+	g_signal_connect(button, "clicked", G_CALLBACK(print_hello), NULL);
+
+	/* Place the second button in the grid cell (1, 0), and make it fill
+	* just 1 cell horizontally and vertically (ie no spanning)
+	*/
+	gtk_grid_attach (GTK_GRID(grid), button, 1, 0, 1, 1);
+
+	button = gtk_button_new_with_label("Quit");
+	g_signal_connect_swapped(button, "clicked", G_CALLBACK (gtk_widget_destroy), window);
+
+	/* Place the Quit button in the grid cell (0, 1), and make it
+	* span 2 columns.
+	*/
+	gtk_grid_attach(GTK_GRID(grid), button, 0, 1, 2, 1);
+
+	/* Now that we are done packing our widgets, we show them all
+	* in one go, by calling gtk_widget_show_all() on the window.
+	* This call recursively calls gtk_widget_show() on all widgets
+	* that are contained in the window, directly or indirectly.
+	*/
+	gtk_widget_show_all(window);
 }
- 
-int main( int   argc,char *argv[] )
+
+int main(int argc, char **argv)
 {
- 
-   GtkWidget *window;
-   GtkWidget *button;
-   gtk_init (&argc, &argv);
- 
-   window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
- 
-   g_signal_connect (window, "destroy",
-   G_CALLBACK (destroy), NULL);
-   /* Let's set the border width of the window to 20.
-    * You may play with the value and see the
-    * difference. */
-   gtk_container_set_border_width
-      (GTK_CONTAINER (window), 20);
- 
-   button = gtk_button_new_with_label ("Click Me!");
- 
-   g_signal_connect (GTK_OBJECT(button),
-      "clicked",G_CALLBACK (greet),
-      "button");
- 
-   gtk_container_add (GTK_CONTAINER (window), button);
- 
-   gtk_widget_show_all(window); 
- 
-   gtk_main ();
- 
-   return 0;
+	GtkApplication *app;
+	int status;
+
+	app = gtk_application_new("org.gtk.example", G_APPLICATION_FLAGS_NONE);
+	g_signal_connect(app, "activate", G_CALLBACK (activate), NULL);
+	status = g_application_run(G_APPLICATION (app), argc, argv);
+	g_object_unref(app);
+
+	return status;
 }
